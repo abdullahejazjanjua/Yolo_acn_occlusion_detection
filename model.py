61b2d05983d4450928773351f48c5142337a3d0e
abdullahejazjanjua <abdullahejaz822@gmail.com>
2024-12-05 01:21:53 +0500

    Added predictions func in model and detailed test in head.py

--- model.py
+++ model.py
@@ -6,9 +6,33 @@ from model.backbone import *
 class YOLO_ACN(nn.Module):
     def __init__(self, classes, anchors):
         super().__init__()
+        self.anchors = anchors
+        self.classes = classes
         self.backbone = BackBone()
         self.neck = neck()
         self.head = head(classes, anchors)
+
+    def _process_output(self, x):
+        low_level_features = x[0]
+        mid_level_features = x[1]
+        high_level_features = x[2]
+
+        # first 8 is responsible for the first box and so on
+        # First row of the first four channels give bbox coordinates
+        # 5th channel is objectness
+        # 6th channel gives class scores
+        # Reshape to to get the form N num_boxes H W Predictions for each anchor box
+
+        high_level_features = einops.rearrange(high_level_features, \
+                                               "N C H W -> N (num_boxes) H W P", num_boxes = self.anchors)
+        
+        bbox = high_level_features[..., :4] # Bbox coordinated for each box
+        objectness_score = high_level_features[..., 4:5] # Bbox coordinated for each box
+        classification_score = high_level_features[..., 5:] # Bbox coordinated for each box
+        
+
+
+
     
     def forward(self, x):
         x = self.backbone(x)
--- model/head.py
+++ model/head.py
@@ -21,11 +21,20 @@ class head(nn.Module):
 if __name__ == "__main__":
     neck_output = (torch.randn((2, 384, 52, 52)), torch.randn((2, 768, 26, 26)),\
          torch.randn((2, 512, 13, 13)))
-    head = head(80)
+    head = head(3)
     x = head(neck_output)
     print(f"x3 shape: {x[0].shape}")
     print(f"x4 shape: {x[1].shape}")
     print(f"Output shape: {x[2].shape}")
+    hll_f = einops.rearrange(x[2], "N (num_boxes P) H W -> N num_boxes H W P", num_boxes = 3)
+    print(f"hll_f shape: {hll_f.shape}")
+    bbox = hll_f[0][0][1][5][:4]
+    objectness_score = hll_f[0][0][1][5][4:5]
+    classification_score = hll_f[0][0][1][5][5:] 
+    print("For the 0th box:")
+    print(f"bbox coordinates: {bbox}")
+    print(f"Objectness score: {objectness_score}")
+    print(f"Classification score: {classification_score}")
 
         
         
